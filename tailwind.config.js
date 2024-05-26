ba364fdce7767eb8e606ff965c1e37c89867c4bb
Jonas Saraiva <148899469+jonascsaraiva@users.noreply.github.com>
2024-05-26 17:19:14 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      colors: {
+        'fundo': '#000E24',   
+        'azul-celeste': '#00AFF2',   
+        'azul-turquesa': '#008EC4',   
+        'white': '#FFFFFF',  
+        'black': '#000000',    
+        'gray': '#F2F2F2',   
+        'border-gray': '#BEBEBE',    
+        'more-gray': '#EDEDED', 
+        'brigth-gray': '#EEEEEE', 
+        'dark-gray': '#9D9D9D', 
+        'transparent': 'transparent',
+      },
+    },
+    plugins: [],
+  }
